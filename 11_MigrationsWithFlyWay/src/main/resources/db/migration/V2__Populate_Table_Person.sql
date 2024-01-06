ff2535398611d672d9224bc6ea7266a4b4739052
eduardofrohlich <eduardofrohlich121@gmail.com>
2024-01-06 00:00:49 -0300

    Flyway migrations

---
+++ 11_MigrationsWithFlyWay/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.person VALUES (2, 'Person name 20', 'Last name12919', 'Address rando!21', 'Male');
+INSERT INTO public.person VALUES (1, 'Eduardo VINI''12', 'Frohlich', 'Rua XV 14', 'Male');
+INSERT INTO public.person VALUES (4, 'topzeroa', 'Frohl1231ich', 'Rua XV 333', 'Female');
+INSERT INTO public.person VALUES (6, 'CInici', 'nme', 'Rua X333333', 'Male');
+INSERT INTO public.person VALUES (8, 'CInici', 'nme', 'Rua india 102', 'Male');
+INSERT INTO public.person VALUES (9, 'eheseba', 'iran', 'Rua pedreuri', 'Male');
+
