7a6c737d22aa3df7c29107f28ae8688ca96e51e4
eduardofrohlich <eduardofrohlich121@gmail.com>
2024-04-25 23:30:14 -0300

    18.6 create integration tests for XML PersonController

--- 18_APIShieldWithTests/src/test/java/br/com/frohlich/integrationtests/vo/TokenVO.java
+++ 18_APIShieldWithTests/src/test/java/br/com/frohlich/integrationtests/vo/TokenVO.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-@XmlRootElement
+@XmlRootElement(name = "TokenVO")
 public class TokenVO implements Serializable {
     private static final long serialVersionUID = 1L;
     private String username;
