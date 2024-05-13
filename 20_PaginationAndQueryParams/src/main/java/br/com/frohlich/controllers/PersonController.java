16230d87685062646a8aad9d3110eda31517067f
eduardofrohlich <eduardofrohlich121@gmail.com>
2024-05-13 07:47:45 -0300

    20.1 implement PagedModel
    - include page hateoas links (first, prev, self, next, last)

--- 20_PaginationAndQueryParams/src/main/java/br/com/frohlich/controllers/PersonController.java
+++ 20_PaginationAndQueryParams/src/main/java/br/com/frohlich/controllers/PersonController.java
@@ -10,10 +10,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,9 +44,9 @@ public class PersonController {
                     @ApiResponse(responseCode = "500", description = "Internal error"),
             }
     )
-    public ResponseEntity<Page<PersonVO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                                  @RequestParam(value = "size", defaultValue = "12") Integer size,
-                                                  @RequestParam(value = "direction", defaultValue = "asc") String direction
+    public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                                     @RequestParam(value = "size", defaultValue = "12") Integer size,
+                                                                     @RequestParam(value = "direction", defaultValue = "asc") String direction
     ) {
         var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
 
--- 20_PaginationAndQueryParams/src/main/java/br/com/frohlich/services/PersonServices.java
+++ 20_PaginationAndQueryParams/src/main/java/br/com/frohlich/services/PersonServices.java
@@ -8,8 +8,11 @@ import br.com.frohlich.mapper.DozerMapper;
 import br.com.frohlich.model.Person;
 import br.com.frohlich.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +29,10 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    public Page<PersonVO> findAll(Pageable pageable) {
+    @Autowired
+    PagedResourcesAssembler<PersonVO> assembler;
+
+    public PagedModel<EntityModel<PersonVO>> findAll(Pageable pageable) {
         logger.info("Finding all people!");
 
         var personPage = repository.findAll(pageable); //returns a Page
@@ -42,7 +48,12 @@ public class PersonServices {
             return p;
         });
 
-        return personVosPage;
+        Link link = linkTo(
+                methodOn(PersonController.class)
+                        .findAll(pageable.getPageNumber(),
+                                pageable.getPageSize(),
+                                "asc")).withSelfRel();
+        return assembler.toModel(personVosPage, link);
     }
 
     public PersonVO findById(Long id) throws Exception {
