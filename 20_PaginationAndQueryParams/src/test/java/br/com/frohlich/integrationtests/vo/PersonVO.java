2a8f78e04602712563c1ce95fab11ccb0dbba665
eduardofrohlich <eduardofrohlich121@gmail.com>
2024-06-01 14:49:08 -0300

    20.91 update BookControllerJsonTest
    - tests pagination

--- 20_PaginationAndQueryParams/src/test/java/br/com/frohlich/integrationtests/vo/PersonVO.java
+++ 20_PaginationAndQueryParams/src/test/java/br/com/frohlich/integrationtests/vo/PersonVO.java
@@ -11,16 +11,11 @@ public class PersonVO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
-
     private String firstName;
-
-
     private String lastName;
     private String address;
-
     private String gender;
     private Boolean enabled;
-
 
     public PersonVO() {
 
