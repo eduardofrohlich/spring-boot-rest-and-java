abf915edf3f7044a1b11ec620aaff939b57e3fbb
eduardofrohlich <eduardofrohlich121@gmail.com>
2024-06-01 17:16:55 -0300

    21.2 implement multiple files upload

--- 21_FileStorageAndUpload/src/main/java/br/com/frohlich/controllers/FileController.java
+++ 21_FileStorageAndUpload/src/main/java/br/com/frohlich/controllers/FileController.java
@@ -3,7 +3,6 @@ package br.com.frohlich.controllers;
 import br.com.frohlich.data.vo.v1.UploadFileResponseVO;
 import br.com.frohlich.services.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Tag(name = "File Endpoint", description = "File API")
 @RestController
@@ -37,6 +39,13 @@ public class FileController {
 
         return new UploadFileResponseVO(filename, fileDownloadUri,
                 file.getContentType(), file.getSize());
+
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        logger.info("Storing files... ");
+        return Arrays.stream(files).map(this::uploadFile).collect(Collectors.toList());
 
     }
 }
