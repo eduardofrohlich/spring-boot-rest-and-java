0882e6c6d7915bd95ba69756fd8471b576121c4d
eduardofrohlich <eduardofrohlich121@gmail.com>
2024-06-11 20:37:08 -0300

    22.3 adjust on app properties test config

--- 22_ContainerizeApplicationWithDocker/src/test/java/br/com/frohlich/configs/TestConfigs.java
+++ 22_ContainerizeApplicationWithDocker/src/test/java/br/com/frohlich/configs/TestConfigs.java
@@ -2,7 +2,7 @@ package br.com.frohlich.configs;
 
 public class TestConfigs {
 
-    public static final int SERVER_PORT = 80;
+    public static final int SERVER_PORT = 8888;
     public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
     public static final String HEADER_PARAM_ORIGIN = "Origin";
     public static final String CONTENT_TYPE_JSON = "application/json";
